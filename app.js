80eb9802d8d3b77419f13b7bebda31a7eba12bd6
kpnemre <kpnemre51@gmail.com>
2021-04-01 14:59:09 +0300

    Finished

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ document.getElementById('btn').addEventListener('click',fibonacci)
 
 function fibonacci() {
     const n = document.getElementById("sayi").value;
-    const arr= [1,1];
+  
+        const arr= [1,1];
     
-    for (let i=2; i<n+1;i++){
-        arr.push(arr[i-2] + arr[i-1]);
-    }
-    document.getElementById("res").innerHTML = arr[n];   
+        for (let i=2; i<n+1;i++){
+            arr.push(arr[i-2] + arr[i-1]);
+        }
+        document.getElementById("res").innerHTML = arr[n];   
+ 
+
+
 }
 
 
